6a39f6f70487a0c6f2de9efb79af49665ff8f7a6
Danny <ljw5040@gmail.com>
2022-02-02 15:13:07 -0800

    created portfolio folder to start and created react

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+import Intro from './components/intro/intro';
+
+function App() {
+	return (
+		<div className="App">
+			<Intro />
+		</div>
+	);
+}
+
+export default App;
---
+++ portfolio/src/components/intro/intro.jsx
@@ -0,0 +1,10 @@
+const Intro = () => {
+	return (
+		<div className="intro">
+			<div className="intro-left">left</div>
+			<div className="intro-right">right</div>
+		</div>
+	);
+};
+
+export default Intro;
